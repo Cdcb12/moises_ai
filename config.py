66b76cef722d4e0a33e0dc30c9e34d6e9ef1ab5d
CARLOS DANIEL <danielcastilloborgess@gmail.com>
2024-06-22 01:33:05 -0400

    "Updated config.py to add ProductionConfig and modified index.py to use ProductionConfig"

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 from decouple import config
+import secrets
 
-class Config():
-    SECRET_KEY =  config('SECRET_KEY')
+class Config:
+    SECRET_KEY = config('SECRET_KEY')
 
 class DevelopmentConfig(Config):
     DEBUG = True
 
+class ProductionConfig(Config):
+    DEBUG = False
+    SECRET_KEY = secrets.token_hex()  # Genera una clave secreta aleatoria
+
 config = {
-    'development': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
 }
